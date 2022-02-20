c2daee6cda06e3563b01eb2e4d5d3bf688d4a3c4
Alona Parkhatska <alona.parkhatska@intellias.com>
2022-02-20 18:46:59 +0200

    Initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+
+    @Test
+    public void simpleTest() {
+        int a = 2;
+        int b = 1;
+
+        Assert.assertNotEquals(a , b);
+
+    }
+}
